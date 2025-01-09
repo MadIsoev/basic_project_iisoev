db07980c8ec265bec02d6e8b7f3f8b00137feb89
MadIsoev <isoeviso@gmail.com>
2025-01-09 08:37:03 +0500

    Update car_price_model_new_app.py

--- car_price_model_new_app.py
+++ car_price_model_new_app.py
@@ -1,1 +1,38 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+st.title('Предсказание цены на подержанные автомобили Opel')
+
+st.write('Прогнозирование цены основывается на таких признаках, как модель, год выпуска, коробка передач, тип топлива и город, где находится автомобиль.')
+
+# Загрузка данных
+df = pd.read_csv("Opel_data.csv")
+
+# Удаляем разделители тысяч и преобразуем в числовые данные
+df['Year'] = df['Year'].astype(int)
+df['Price'] = df['Price'].astype(int)
+
+# Отображение данных
+with st.expander('Данные'):
+    st.write("Признаки (X):")
+    X_raw = df.drop('Price', axis=1)
+    st.dataframe(X_raw)
+
+    st.write("Целевая переменная (y):")
+    y_raw = df['Price']
+    st.dataframe(y_raw)
+
+# Ввод данных пользователем
+with st.sidebar:
+    st.header("Введите характеристики автомобиля: ")
+    model = st.selectbox('Модель', df['Model'].unique())
+    year = st.slider('Год выпуска', int(df['Year'].min()), int(df['Year'].max()))
+    transmission = st.selectbox('Коробка передач', df['Transmission'].unique())
+    fuel_type = st.selectbox('Тип топлива', df['Fuel type'].unique())
+    city = st.selectbox('Город', df['City'].unique())
+
+
 
