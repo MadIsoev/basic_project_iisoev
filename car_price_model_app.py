5c5cf3d54ce52f76c738972f868f0649583a10ac
MadIsoev <isoeviso@gmail.com>
2025-01-09 07:57:30 +0500

    Update car_price_model_app.py

--- car_price_model_app.py
+++ car_price_model_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 st.title('Предсказание цены на подержанные автомобили Opel')
 
@@ -44,4 +43,106 @@ with st.sidebar:
        'Таджикабад', 'Рогун', 'Нурабад', 'Муминабад', 'Айни', 'Носири Хусрав', 'Джами', 'Лахш (Джиргиталь)',
        'Шамсиддин Шохин (Шуроабад)', 'Вандж', 'Ховалинг', 'Бальджувон', 'Горная Матча', 'Истиклол', 'Дарваз'))
 
+# Визуализация данных
+st.subheader('Визуализация данных')
+fig = px.scatter(df, x='Year', y='Price', color='Fuel type', title='Зависимость цены от года выпуска и типа топлива')
+st.plotly_chart(fig)
 
+fig2 = px.histogram(df, x='Price', nbins=30, title='Распределение цен автомобилей')
+st.plotly_chart(fig2)
+
+## Preprocessing
+data = {
+    'Модель': Model,
+    'Год выпуска': Year,
+    'Коробка передач': Transmission,
+    'Тип топлива': Fuel_type,
+    'Город': City
+}
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander('Input features'):
+    st.write('**Input penguin**')
+    st.dataframe(input_df)
+    st.write('**Combined penguins data** (input row + original data)')
+    st.dataframe(input_penguins)
+
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+# Separate the top row (our input) from the rest
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+# Encode the target
+target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+def target_encode(val):
+    return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+    st.write('**Encoded X (input penguin)**')
+    st.dataframe(input_row)
+    st.write('**Encoded y**')
+    st.write(y)
+
+# Model Training
+param_grid = {
+    'n_estimators': [50, 100],
+    'max_depth': [None, 5, 10]
+}
+
+# Create the base model
+base_rf = RandomForestClassifier(random_state=42)
+
+# Perform grid search
+grid_search = GridSearchCV(base_rf, param_grid, cv=3, scoring='accuracy', n_jobs=-1)
+grid_search.fit(X, y)
+
+best_model = grid_search.best_estimator_
+best_params = grid_search.best_params_
+st.write("**Best Parameters**:", best_params)
+
+# ---------------------------
+# 7) Apply the best model to make predictions
+# ---------------------------
+prediction = best_model.predict(input_row)
+prediction_proba = best_model.predict_proba(input_row)
+
+df_prediction_proba = pd.DataFrame(prediction_proba, columns=['Adelie', 'Chinstrap', 'Gentoo'])
+
+## Print model final results
+
+st.subheader('Predicted Species')
+st.dataframe(
+    df_prediction_proba,
+    column_config={
+        'Adelie': st.column_config.ProgressColumn(
+            'Adelie',
+            format='%f',
+            width='medium',
+            min_value=0,
+            max_value=1
+        ),
+        'Chinstrap': st.column_config.ProgressColumn(
+            'Chinstrap',
+            format='%f',
+            width='medium',
+            min_value=0,
+            max_value=1
+        ),
+        'Gentoo': st.column_config.ProgressColumn(
+            'Gentoo',
+            format='%f',
+            width='medium',
+            min_value=0,
+            max_value=1
+        ),
+    },
+    hide_index=True
+)
+
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.success(f"Predicted species: **{penguins_species[prediction][0]}**")
